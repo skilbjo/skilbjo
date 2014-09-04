9ab6e4ec352563f1252a28a031eaa6214d821cb3
skilbjo <john.skilbeck@gmail.com>
2014-09-04 13:40:13 -0700

    heroku && new relic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['stuff'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '7ffb636620c834a5527ad3c27d2b4d2dcf6bdb15',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};

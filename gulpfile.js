f3ecee0b77b6fd74a90bcc0c0c4aafdbd7c4d966
skilbjo <john.skilbeck@gmail.com>
2014-09-12 00:37:40 -0700

    stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,33 @@
 var gulp 						= require('gulp')
 	, jshint 					= require('gulp-jshint')
-	, stylish 			 	= require('jshint-stylish');
+	, stylish 			 	= require('jshint-stylish')
+	, spawn    				= require('child_process').spawn
+	, node
+	, jsLocations   = ['server.js'
+		, 'app/**/**/*.js'
+		, 'public/js/merchant/*.js'
+		, 'public/js/transaction/*.js']
+  , cssLocations  = ['public/css/*.css']
+  , jadeLocations = ['app/view/**/*.jade']
+  , viewLocations = ['app/view/**/.html'];
 
-gulp.task('default', function() {
- 
-});
+gulp.task('default',
+ ['lint']
+);
 
 gulp.task('lint', function() {
-	return gulp.src(['server.js'
-		, 'app/**/**/*.js'
-    , 'test/*.js'])
-		.pipe(jshint())
+	return gulp.src(jsLocations)
+		.pipe(jshint({laxcomma:true}))
 		.pipe(jshint.reporter(stylish));
+});
+
+// not working because env variables haven't been loaded
+gulp.task('server', function() {
+  if (node) node.kill();
+  node = spawn('node', ['server.js'], {stdio: 'inherit'});
+  node.on('close', function (code) {
+    if (code === 8) {
+      gulp.log('Error detected, waiting for changes...');
+    }
+  });
 });
